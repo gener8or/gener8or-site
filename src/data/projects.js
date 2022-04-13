231f0ef93428914983fa563963ded1999de75c5b
ajm <alecjohnmiller@gmail.com>
2022-04-13 02:25:49 -0400

    commit on main

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,30 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Maps',
-    subtitle: 'Plotting pixels',
+    title: 'Maps & Location Analytics',
+    subtitle:,
     link: 'https://overviewanalytics.shinyapps.io/shiny-heatmap/',
     image: '/images/projects/kepler.gl copy.png',
-    date: '2022-04-10',
     desc:
-      '',
+      'Building maps and dashboards to display location data in powerful, accessible ways.',
   },
+  {
+    title: 'Edge & IoT Systems',
+    subtitle:,
+    link: '/',
+    image: '/',
+    desc:
+      'Building data processing pipelines for imagery, environmental, and location data.',
+  },
+  {
+    title: 'Data Stratey & Governance',
+    subtitle: 'Data Lakehouse Consulting',
+    link: '/',
+    image: '/',
+    desc:
+      'Building Data Lakehouses with Google [Biglake]('https://www.nextplatform.com/2022/04/06/google-biglake-stretches-bigquery-across-all-data/')
+  },
+
 ];
 
 export default data;
