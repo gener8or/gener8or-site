455175df17a32db0f35b282cd7c8fa88038d65e9
ajm <alecjohnmiller@gmail.com>
2022-04-13 01:00:27 -0400

    commit on main

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -39,7 +39,7 @@ Main.defaultProps = {
   children: null,
   fullPage: false,
   title: null,
-  description: "Overview Analytics website.",
+  description:'Overview Analytics website.',
 };
 
 export default Main;
