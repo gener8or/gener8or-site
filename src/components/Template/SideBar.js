56b6b7a301637b30c5b7f0d270504cbbd5f109ce
ajm <alecjohnmiller@gmail.com>
2022-04-12 23:35:34 -0400

    commit on main

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,14 +19,7 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Overview Analytics is a Toronto based data &amp; analytics company.
       </p>
       <ul className="actions">
         <li>
@@ -37,7 +30,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Overview Analytics <Link to="/">overviewanalytics.ca</Link>.</p>
     </section>
   </section>
 );
