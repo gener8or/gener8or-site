2b28304c78a8c30af9275497a8d8f07ee5cd5fd2
ajm <alecjohnmiller@gmail.com>
2022-04-13 01:16:23 -0400

    commit on main

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ const data = [
     title: 'Maps',
     subtitle: 'Plotting pixels',
     link: 'https://overviewanalytics.shinyapps.io/shiny-heatmap/',
-    image: 'public/images/projects/kepler.gl copy.png',
+    image: '/images/projects/kepler.gl copy.png',
     date: '2022-04-10',
     desc:
       '',
