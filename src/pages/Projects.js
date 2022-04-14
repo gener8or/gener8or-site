283b11bc277db01c0e8f1426a28ae9a15bd691f9
ajm <alecjohnmiller@gmail.com>
2022-04-13 20:58:11 -0400

    commit on main

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Carousel from 'react-bootstrap/Carousel'
 
 import Main from '../layouts/Main';
 
 import Cell from '../components/Projects/Cell';
-import data from '../data/projects';  
+import data from '../data/projects';
 
 const Projects = () => (
   <Main
@@ -19,41 +18,12 @@ const Projects = () => (
           <p>Services offered by Overview Analytics</p>
         </div>
       </header>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=First slide&bg=373940"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
-              alt="Second slide"
-            />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
-              alt="Third slide"
-            />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+      {data.map((project) => (
+        <Cell
+          data={project}
+          key={project.title}
+        />
+      ))}
     </article>
   </Main>
 );
