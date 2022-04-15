38948f2ef0be5948481fa495838a8f3021a83c87
ajm <alecjohnmiller@gmail.com>
2022-04-15 10:55:12 -0400

    commit on main

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -9,13 +9,13 @@ const Index = () => (
   >
     <article className="post" id="index">
       <header>
-        {/* <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+        <div className="title">
+          {/* <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
           <p>
             A beautiful, responsive, statically-generated,
             react application written with modern Javascript.
-          </p>
-        </div> */}
+          </p> */}
+        </div>
       </header>
       <p> Welcome to Overview Analytics. We advise on advanced analytics and data strategy
         for a variety of clients in the GTA and Waterloo region.
@@ -23,7 +23,7 @@ const Index = () => (
         <Link to="/projects">services</Link>, {' '}
         or <Link to="/contact">contact</Link> us.
       </p>
-      {/* <p> Code <a href="https://github.com/overviewanalytics/personal-site">here</a>.</p> */}
+      <p> Code <a href="https://github.com/overviewanalytics/personal-site">here</a>.</p>
     </article>
   </Main>
 );
