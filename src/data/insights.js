23c9cc142433de04d9e1c01159183b5a63c6f034
ajm <alecjohnmiller@gmail.com>
2022-10-24 16:54:17 -0400

    updated insights page

--- src/data/insights.js
+++ src/data/insights.js
@@ -1,12 +1,12 @@
 // TODO Add a couple lines about each project
 const data = [
-  {
-    title: 'Modelling Traffic Flow Using AI',
-    // subtitle:,
-    link: 'https://1drv.ms/b/s!Aj_UH9guULHtjswlP6EzVda6RTcl_w',
-    image: '/images/projects/trafficflow.png',
-    desc: 'Analyze traffic patterns using nVidia Deepstream SDK & Jetson Edge IoT Hardware',
-  },
+  // {
+  //   title: 'Modelling Traffic Flow Using AI',
+  //   // subtitle:,
+  //   link: 'https://1drv.ms/b/s!Aj_UH9guULHtjswlP6EzVda6RTcl_w',
+  //   image: '/images/projects/trafficflow.png',
+  //   desc: 'Analyze traffic patterns using nVidia Deepstream SDK & Jetson Edge IoT Hardware',
+  // },
   {
     title: 'Lift & Shift',
     // subtitle:,
