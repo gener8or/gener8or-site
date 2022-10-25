3b39b96161cf15f17673811d43c0fd981d6285dc
ajm <alecjohnmiller@gmail.com>
2022-10-24 21:30:45 -0400

    fixed test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -12,7 +12,7 @@ import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Index from '../pages/Index';
 import NotFound from '../pages/NotFound';
-import Projects from '../pages/Projects';
+import Services from '../pages/Services';
 import Resume from '../pages/Resume';
 import Stats from '../pages/Stats';
 
