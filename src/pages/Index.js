9a57663ed4cba9a4af2861dac7b60383be0f9181
Alec Miller <alec@overviewanalytics.ca>
2022-10-25 22:50:16 -0400

    updated about us

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -10,15 +10,19 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <h2 data-testid="heading"><Link to="/">About Us</Link></h2>
           {/* <p>
             A beautiful, responsive, statically-generated,
             react application written with modern Javascript.
           </p> */}
         </div>
       </header>
-      <p> Welcome to Overview Analytics. We advise on advanced analytics and data strategy
-        for a variety of clients in the GTA and Waterloo region.
+      <p>        
+        Overview Analytics is a data & analytics service provider. We advise small & mid-market businesses on data strategy and do custom-builds of data pipelines for a variety of technology-based businesses. 
+        Our areas of expertise include commercialization/IP, data governance, business intelligence, location analytics, data models, pipelines, and dashboards.
+        We help clients unlock the value of their data by pairing deep business strategy with bleeding-edge AI models such as Open AI GPT-3, Github Co-Pilot, and H2O AutoML. 
+        We build custom data processing pipelines for our clients with a focus on semi/unstructured data such as image, audio, video, and text. 
+
         Learn more <Link to="/about">about us</Link>, check out our {' '}
         <Link to="/services">services</Link>, {' '}
         or <Link to="/contact">contact</Link> us.
