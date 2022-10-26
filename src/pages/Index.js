e466d871a792931897e2183e39f4d0f97c1e635b
Alec Miller <alec@overviewanalytics.ca>
2022-10-26 13:44:42 -0400

    udpated index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -21,17 +21,14 @@ const Index = () => (
         Overview Analytics is a data & analytics service provider. We advise small & mid-market
         businesses on data strategy and do custom-builds of data pipelines for a variety of
         technology-based businesses.
-        
         Our areas of expertise include commercialization/IP, data governance,
         business intelligence, location analytics, data models, pipelines,
-        and dashboards. We help clients unlock the value of their data by 
+        and dashboards. We help clients unlock the value of their data by
         pairing deep business strategy with bleeding-edge AI models such as
         Open AI GPT-3, Github Co-Pilot, and H2O AutoML.
-
         We build custom data processing pipelines for our clients
         with a focus on semi/unstructured data such as image, audio,
         video, and text.
-        
         Learn more <Link to="/about">about us</Link>, check out our {' '}
         <Link to="/services">services</Link>, {' '}
         or <Link to="/contact">contact</Link> us.
