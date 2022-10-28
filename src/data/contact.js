52e87538fc83924ee414a4b34a68606d9165c99f
Alec Miller <alec@overviewanalytics.ca>
2022-10-28 02:28:32 -0400

    please work

--- src/data/contact.js
+++ src/data/contact.js
@@ -5,38 +5,36 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 const data = [
-
-  {
-    link: 'mailto:gener8ive@gmail.com',
-    label: 'Email',
-    icon: faEnvelope,
-  },
-  {
-    link: 'https://twitter.com/gener8ive',
-    label: 'Twitter',
-    icon: faTwitter,
-  },
-  {
-    link: 'https://instagram.com/gener8ive',
-    label: 'Instagram',
-    icon: faInstagram,
-  },
-  {
-    link: 'https://linktr.ee/gener8or',
-    label: 'Linktree',
-    icon: faPaypal,
-  },
-  {
-    link: 'https://medium.com/gener8ive',
-    label: 'Medium',
-    icon: faMedium,
-  },
-  {
-    link: 'https://www.youtube.com/channel/UCmxa6mDOmtdHOWzKCwcYtBQ',
-    label: 'Youtube',
-    icon: faYoutube,
-  },
-
+    {
+      link: 'mailto:gener8ive@gmail.com',
+      label: 'Email',
+      icon: faEnvelope,
+    },
+    {
+      link: 'https://twitter.com/gener8ive',
+      label: 'Twitter',
+      icon: faTwitter,
+    },
+    {
+      link: 'https://instagram.com/gener8ive',
+      label: 'Instagram',
+      icon: faInstagram,
+    },
+    {
+      link: 'https://linktr.ee/gener8or',
+      label: 'Linktree',
+      icon: faPaypal,
+    },
+    {
+      link: 'https://medium.com/gener8ive',
+      label: 'Medium',
+      icon: faMedium,
+    },
+    {
+      link: 'https://www.youtube.com/channel/UCmxa6mDOmtdHOWzKCwcYtBQ',
+      label: 'Youtube',
+      icon: faYoutube,
+    },
 ];
 
 export default data;
