1a5a6c271ca184c1a340660e0be5049c7a678943
Alec Miller <alec@overviewanalytics.ca>
2022-10-28 02:12:21 -0400

    updated socials

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,13 +1,5 @@
 import { faInstagram, faMedium, faPaypal, faYoutube } from '@fortawesome/free-brands-svg-icons';
-
-// import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
-
-// import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-
-// import { faSquare } from '@fortawesome/free-regular-svg-icons';
-
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
