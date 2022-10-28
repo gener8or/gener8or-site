6d6d55edd6c5e08a52bda51383d7638f1504978c
Alec Miller <alec@overviewanalytics.ca>
2022-10-28 01:36:25 -0400

    updated socials

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,39 +1,46 @@
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faMedium, faPaypal, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
-// import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { faSquare } from '@fortawesome/free-regular-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 const data = [
+ 
   {
-    link: 'https://github.com/overviewanalytics',
-    label: 'Github',
-    icon: faGithub,
-  },
-  {
-    link: 'mailto:info@overviewanalytics.ca',
+    link: 'mailto:gener8ive@gmail.com',
     label: 'Email',
     icon: faEnvelope,
   },
   {
-    link: 'https://twitter.com/oa_analytics',
+    link: 'https://twitter.com/gener8ive',
     label: 'Twitter',
     icon: faTwitter,
   },
   {
-    link: 'https://overviewanalytics.square.site',
-    label: 'Square Appointments',
-    icon: faSquare,
-  },
-  {
-    link: 'https://instagram.com/overviewanalytics',
+    link: 'https://instagram.com/gener8ive',
     label: 'Instagram',
     icon: faInstagram,
   },
+  {
+    link: 'https://linktr.ee/gener8or',
+    label: 'Linktree',
+    icon: faPaypal,
+  },
+  {
+    link: 'https://medium.com/gener8ive',
+    label: 'Medium',
+    icon: faMedium,
+  },
+  {
+    link: 'https://www.youtube.com/channel/UCmxa6mDOmtdHOWzKCwcYtBQ',
+    label: 'Youtube',
+    icon: faYoutube,
+  },
 
+  
 ];
 
 export default data;
