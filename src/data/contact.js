fbd70fafaadc8f28b59c5996acbd8122bc048fe8
Alec Miller <alec@overviewanalytics.ca>
2022-10-28 01:46:07 -0400

    fixed contact.js

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,12 +1,13 @@
 import { faInstagram, faMedium, faPaypal, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+// import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faSquare } from '@fortawesome/free-regular-svg-icons';
+// import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+// import { faSquare } from '@fortawesome/free-regular-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
+
 const data = [
   {
     link: 'mailto:gener8ive@gmail.com',
