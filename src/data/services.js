7045a09b577b3cd00160759ae0b2e69c8d3dc649
Alec Miller <alec@overviewanalytics.ca>
2022-11-01 02:00:56 -0400

    added images to gallery

--- src/data/services.js
+++ src/data/services.js
@@ -1,37 +1,48 @@
 // TODO Add a couple lines about each project
 const data = [
-  // {
-  //   title: 'Data Strategy & Governance',
-  //   subtitle: 'Data Lakehouse Consulting',
-  //   link: 'https://cloud.google.com/blog/products/data-analytics/unifying-data-lakes-and-data-warehouses-across-clouds-with-biglake',
-  //   image: '/images/projects/biglake.png',
-  //   desc:
-  //     'Building Data Lakehouses with Google Biglake',
-  // },
-  // {
-  //   title: 'Maps & Location Analytics',
-  //   // subtitle:,
-  //   link: 'https://overviewanalytics.shinyapps.io/shiny-heatmap/',
-  //   image: '/images/projects/keplergl.jpeg',
-  //   desc:
-  //     'Building maps and dashboards to display location data in powerful, accessible ways.',
-  // },
-  // {
-  //   title: 'Edge & IoT Systems',
-  //   // subtitle: ,
-  //   link: 'https://developer.nvidia.com/deepstream-sdk',
-  //   image: '/images/projects/ds-workflow.png',
-  //   desc:
-  //     'Building data processing pipelines for imagery, environmental, and location data.',
-  // },
-  // {
-  //   title: 'Tech Stack',
-  //   subtitle: 'Gener8or Studios Technology Stack',
-  //   link: '',
-  //   image: '/images/projects/OA_techstack.png',
-  //   desc:
-  //     'Technologies used by Gener8or Studios in client engagements',
-  // },
+  {
+    link: '',
+    image: '/images/projects/DALL·E 2022-10-20 08.20.43 - an ink painting of the toronto skyline in the style of Wu Guanzhong with the skydome.png',
+  },
+  {
+    title: '',
+    subtitle: '',
+    link: '',
+    image: '/images/projects/20221030210710_00009_a_vibrant_painting_of_a_bathroom_sink_on_top_of_a_pedestal_in_the_style_of_east_asian_cyberpunk_art_brutalist_monumental_architecture_dramatic_and_cinematic_lig.png',
+    desc:
+      '',
+  },
+  {
+    title: '',
+    subtitle: '',
+    link: '',
+    image: '/images/projects/2916587357_a_beautiful_1970_s_neighbourhood_scene_of_kensington_market_in_toronto_in_the_style_of_cyberpunk_2049__digital_art__8k__highly_detailed__cinematic_lighting__vaporwave_and_synthwave__highly_coherent__beeple__rtx_on__tokyo_city_lig.png',
+    desc:
+      '',
+  },
+  {
+    title: '',
+    subtitle: '',
+    link: '',
+    image: '/images/projects/20221002153507_00186_an_amazing_black_and_white_selfportrait_of_sigmund_freud_in_the_style_of_a_masterful_renaissance_painting_rembrandt_and_caravaggio_and_banksy_matte_finish_drama.png',
+  },
+  {
+    title: '',
+    subtitle: '',
+    link: '',
+    image: '/images/projects/a sublime painting of a lone pine tree overlooking a lake and forest, glowing in the early evening light, in the style of Tom Thomson and Thomas Kinkade.png',
+  },
+  {
+    title: '',
+    subtitle: '',
+    link: '',
+    image: '/images/projects/20221028010128_00016_a_train_of_thoughts_through_the_mind_skull_with_the_most_beautiful_eyes_in_the_entire_world_transcending_suffering_incredible_mindexpanding_and_aweinspiring_art.png',
+  },
+  {
+    title: '',
+    subtitle: '',
+    image: '/images/projects/DALL·E 2022-07-26 16.03.50 - Unthinkable complexity, electrical circuitry, firing and wiring, clusters and constellations of data, in the style of colorful digital art, in the sha.jpeg',
+  },
 ];
 
 export default data;
