6ed7be83affc3fbb494bedb83744f757c628843e
Alec Miller <alec@overviewanalytics.ca>
2022-10-31 23:39:56 -0400

    updated projects

---
+++ src/data/projects.js
@@ -0,0 +1,11 @@
+// TODO Add a couple lines about each project
+const data = [
+  {
+    title: 'Toronto in the Style of Wu Guanzhong',
+    subtitle: '2022 Promptathon',
+    image: '/images/projects/DALL·E 2022-10-20 08.20.43 - an ink painting of the toronto skyline in the style of Wu Guanzhong with the skydome.png',
+    date: '2022-10-31',
+  },
+];
+
+export default data;
